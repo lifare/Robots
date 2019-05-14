1244375760bf4c0dd5184de91f9c36d43b757233
MariaBag <43343526+MariaBag@users.noreply.github.com>
2019-05-14 15:37:19 +0500

    Add files via upload

--- robots/src/gui/RobotsProgram.java
+++ robots/src/gui/RobotsProgram.java
@@ -3,45 +3,54 @@ package gui;
 import java.awt.Frame;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.beans.PropertyVetoException;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
-public class RobotsProgram
-{
-    public static void main(String[] args) {
-      try {
-        UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-//        UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-//        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-//        UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-      SwingUtilities.invokeLater(() -> {
-        MainApplicationFrame frame = new MainApplicationFrame();
+public class RobotsProgram {
 
-        frame.addWindowListener(new WindowAdapter() {
-        	public void windowClosing(WindowEvent event) {
-				Object[] options = { "Да", "Нет!" };
-				int n = JOptionPane
-						.showOptionDialog(event.getWindow(), "Закрыть окно?",
-								"Подтверждение", JOptionPane.YES_NO_OPTION,
-								JOptionPane.QUESTION_MESSAGE, null, options,
-								options[0]);
-				if (n == 0) {
-					event.getWindow().setVisible(false);
-					frame.unregister();
-					System.exit(0);
-				}
-				frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-			}
-        });
+	public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+//          UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+//          UIManager.setLookAndFeel(UIsManager.getSystemLookAndFeelClassName());
+//          UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+        SwingUtilities.invokeLater(() -> {
+            MainApplicationFrame frame;
+			try {
+				frame = new MainApplicationFrame();
+			
+				frame.addWindowListener(new WindowAdapter() {
+	        	    public void windowClosing(WindowEvent event) {
+					    Object[] options = { "Да", "Нет!" };
+					    int n = JOptionPane
+					    		.showOptionDialog(event.getWindow(), "Закрыть окно?",
+					    				"Подтверждение", JOptionPane.YES_NO_OPTION,
+					    				JOptionPane.QUESTION_MESSAGE, null, options,
+					    				options[0]);
+					  if (n == 0) {
+						  event.getWindow().setVisible(false);
+						  WindowSerializer.saveWindowState(frame.logWindow, "log_window.bin");
+						  WindowSerializer.saveWindowState(frame.gameWindow, "game_window.bin");
+						  frame.unregister();
+						  System.exit(0);
+					  }
+					  frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+				    }
+	            });
   
-        frame.pack();
-        frame.setVisible(true);
-        frame.setExtendedState(Frame.MAXIMIZED_BOTH);
-      });
-    }}
+	            frame.pack();
+	            frame.setVisible(true);
+	            frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+	        } catch (PropertyVetoException e) {
+				e.printStackTrace();
+			}
+		});
+    }
+}
